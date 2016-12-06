5b8783464f7c6894b14b6be59346fe8f5766df29
Robin Ricard <ricard.robin@gmail.com>
2016-12-06 15:31:44 +0100

    Multi-file example

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,26 @@ const graphqlDocuments = require('./documents.json');
 
 const client = new ApolloClient();
 
-client.query({
-  query: graphqlDocuments['Feed.graphql']
-})
-.then(({data}: {data: FeedQuery}) => {
-  document.getElementById("app").innerHTML = `
-    <h1>GitHunt Feed</h1>
-    <ul>
-      ${data.feed.map(({repository, postedBy}) => `
-        <li>
-          <a href="https://github.com/${repository.owner.login}/${repository.name}">
-            ${repository.owner.login}/${repository.name}
-          </a>
-          <span> - posted by: </span>
-          <a href="https://github.com/${postedBy.login}">${postedBy.login}</span>
-        </li>
-      `).join('\n')}
-    </ul>
-  `;
-})
+function renderApp() {
+  client.query({
+    query: graphqlDocuments['Feed.graphql']
+  })
+  .then(({data}: {data: FeedQuery}) => {
+    document.getElementById("app").innerHTML = `
+      <h1>GitHunt Feed</h1>
+      <ul>
+        ${data.feed.map(({repository, postedBy}) => `
+          <li>
+            <a href="https://github.com/${repository.owner.login}/${repository.name}">
+              ${repository.owner.login}/${repository.name}
+            </a>
+            <span> - posted by: </span>
+            <a href="https://github.com/${postedBy.login}">${postedBy.login}</span>
+          </li>
+        `).join('\n')}
+      </ul>
+    `;
+  });
+}
+
+renderApp();
